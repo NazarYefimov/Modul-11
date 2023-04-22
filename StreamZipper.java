a231a13b4daaa62b2f6b76e17ca947f818e0eaaf
NazarYefimov <123975599+NazarYefimov@users.noreply.github.com>
2023-04-23 00:23:58 +0300

    Update StreamZipper.java

--- StreamZipper.java
+++ StreamZipper.java
@@ -1,20 +1,18 @@
+import java.util.Iterator;
 import java.util.stream.Stream;
 
 class StreamZipper {
-
     public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
-        return Stream.concat(
-                first.map(i -> new Object[]{1, i}),
-                second.map(i -> new Object[]{2, i}))
-                .sorted((o1, o2) -> Integer.compare((int) o1[0], (int) o2[0]))
-                .map(o -> (T) o[1]);
+        Iterator<T> secondIterator = second.iterator();
+        Stream<T> zippedStream = first.flatMap(x -> secondIterator.hasNext() ? Stream.of(x, secondIterator.next()) : Stream.empty());
+        return zippedStream;
     }
 
     public static void main(String[] args) {
-        Stream<Integer> first = Stream.of(1, 2, 3, 4);
-        Stream<Integer> second = Stream.of(5, 6, 7, 8);
-
-        Stream<Integer> result = zip(first, second);
-        result.forEach(System.out::println);
+        Stream<Integer> stream1 = Stream.of(1, 2, 3, 4, 5);
+        Stream<Integer> stream2 = Stream.of(6, 7, 8, 9, 10, 11);
+        
+        Stream<Integer> zippedStream = zip(stream1, stream2);
+        zippedStream.forEach(System.out::println); 
     }
 }
