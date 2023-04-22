4a2e934866ca119abef1c196b7475f1e7011ad3f
NazarYefimov <123975599+NazarYefimov@users.noreply.github.com>
2023-04-23 00:59:28 +0300

    Update SortAndUppercaseNames.java

--- SortAndUppercaseNames.java
+++ SortAndUppercaseNames.java
@@ -1,35 +1,20 @@
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Collections;
+
 class SortAndUppercaseNames {
     public static void main(String[] args) {
-        
-        List<String> names = new ArrayList<>();
-        names.add("Ivan");
-        names.add("Peter");
-        names.add("Oksana");
-        names.add("Andrii");
-        names.add("Maria");
-        names.add("Maxim");
+       
+        List<String> names = List.of("Ivan", "Peter", "Oksana", "Andrii", "Maria", "Maxim");
 
-        
         List<String> result = sortAndUppercase(names);
         System.out.println(result);
     }
 
     public static List<String> sortAndUppercase(List<String> names) {
-      
-        List<String> result = new ArrayList<>(names);
-
-        // Перетворюємо всі імена в верхній регістр
-        for (int i = 0; i < result.size(); i++) {
-            String name = result.get(i);
-            result.set(i, name.toUpperCase());
-        }
-
-        // Сортуємо імена за спаданням
-        Collections.sort(result, Collections.reverseOrder());
-
-        return result;
+        return names.stream()
+                .map(String::toUpperCase) 
+                .sorted(Collections.reverseOrder()) 
+                .collect(Collectors.toList()); 
     }
 }
